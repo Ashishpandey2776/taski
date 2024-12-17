8e87257f2a16f22958584cfbd191be2db709e13f
Ashish Kumar <ashishkumar748867@gmail.com>
2024-12-17 16:56:50 +0530

    becryptjs add

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,5 @@
 import { User } from "../modals/user.model.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken";
 import { cookieSet } from "../utils/feacture.js";
 
